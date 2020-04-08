6e28cad3c8fa6a3c1f35fd72ce5606c9a15c596d
coospro <zhuliang@coos.top>
2020-04-08 20:30:13 +0800

    合并

--- plugin.app/src/main/resources/resources/index.js
+++ plugin.app/src/main/resources/resources/index.js
@@ -5311,18 +5311,67 @@ var Editor = function(options) {
 	var PageEditor = coos.createClass(Editor);
 	Editor.Page = PageEditor;
 
+	let layout_map = {};
+	PageEditor.prototype.buildPageView = function($box) {
+		layout_map = {};
+		let root = this.getLayoutRoot();
+		let id = this.getLayoutID(root);
+		$box.attr('layout-id', id);
+		layout_map[id] = root;
+		let $view = this.appendLayoutView($box, root);
+
+		new Vue({
+			el : $view[0],
+			data : {}
+		})
+	};
+
+	PageEditor.prototype.getLayoutID = function(layout) {
+		let root = this.getLayoutRoot();
+		if (root == layout) {
+			return 0;
+		} else {
+			return coos.getNumber();
+		}
+	};
+
+	PageEditor.prototype.appendLayoutView = function($parent, layout) {
+		if (!layout) {
+			return;
+		}
+		let $view = null;
+		let id = this.getLayoutID(layout);
+		layout_map[id] = layout;
+		if (coos.isEmpty(layout.template)) {
+			if (coos.isEmpty(layout.html)) {
+				$view = $('<div ></div>');
+			} else {
+				$view = $(layout.html);
+			}
+		} else {
+			$view = $(layout.template);
+		}
+		$view.attr('layout-id', id);
+
+		$parent.append($view);
+
+		if (layout.layouts) {
+			layout.layouts.forEach(one => {
+				this.appendLayoutView($view, one);
+			});
+		}
+		return $view;
+	};
 	PageEditor.prototype.buildDesign = function() {
 		var that = this;
 		var $design = this.$design;
-		$design.empty();
 
-		let model = this.model;
-
-		let layout = model.layout || {};
-
+		$design.find('.page-design-box:first').remove();
 		var $box = $('<div class="page-design-box"></div>');
 		$design.append($box);
 
+		this.buildPageModel($design);
+		this.buildPageView($box);
 		this.bindPageDesignEvent($box);
 
 	};
@@ -5339,21 +5388,148 @@ var Editor = function(options) {
 
 	};
 
+	PageEditor.prototype.getLayoutFromEl = function(el) {
+		el = $(el);
+		if (el.length == 0) {
+			return null;
+		}
+		if (coos.isNotEmpty(el.attr('layout-id'))) {
+			return layout_map[el.attr('layout-id')];
+		}
+		return this.getLayoutFromEl(el.parent());
+	};
+	PageEditor.prototype.getLayoutRoot = function() {
+		this.model.layout = this.model.layout || {};
+		return this.model.layout;
+	};
+	PageEditor.prototype.getLayoutParent = function(layout, p) {
+		let root = this.getLayoutRoot();
+
+		if (layout == root) {
+			return null;
+		}
+		if (p == null) {
+			p = root;
+		}
+		let res = null;
+		if (p.layouts) {
+			if (p.layouts.indexOf(layout) >= 0) {
+				res = p;
+			} else {
+				p.layouts.forEach(one => {
+					if (res == null) {
+						res = this.getLayoutParent(one);
+					}
+				});
+			}
+		}
+		return res;
+	};
 	PageEditor.prototype.showPageDesignContextmenu = function(event) {
+		let that = this;
 		var eventData = {
 			clientX : event.clientX,
 			clientY : event.clientY
 		};
 		var menus = [];
 
+		let model = this.model;
+
+		let layout = this.getLayoutFromEl(event.target);
+		if (layout == null) {
+			return;
+		}
+		let parentLayout = this.getLayoutParent(layout);
+
 		menus.push({
 			text : "添加",
-			onClick : function() {}
+			onClick : function() {
+				that.choosePageModel(function(res) {
+					that.recordHistory();
+					layout.layouts = layout.layouts || [];
+					layout.layouts.push(res);
+					that.changeModel();
+				});
+
+			}
+		});
+		if (parentLayout) {
+			menus.push({
+				text : "前边添加",
+				onClick : function() {
+					that.choosePageModel(function(res) {
+						that.recordHistory();
+						parentLayout.layouts.splice(0, 0, res);
+						that.changeModel();
+					});
+
+				}
+			});
+			menus.push({
+				text : "后边添加",
+				onClick : function() {
+					that.choosePageModel(function(res) {
+						that.recordHistory();
+						parentLayout.layouts.push(res);
+						that.changeModel();
+					});
+
+				}
+			});
+		}
+		menus.push({
+			text : "上移",
+			onClick : function() {
+				if (parentLayout == null) {
+					parentLayout = that.getLayoutRoot();
+				}
+
+				let list = parentLayout.layouts;
+				let index = list.indexOf(layout);
+				if (index > 0) {
+					that.recordHistory();
+					list[index] = list.splice(index - 1, 1, list[index])[0];
+					that.changeModel();
+				}
+			}
+		});
+		menus.push({
+			text : "下移",
+			onClick : function() {
+				if (parentLayout == null) {
+					parentLayout = that.getLayoutRoot();
+				}
+
+				let list = parentLayout.layouts;
+				let index = list.indexOf(layout);
+				if (index < list.length - 1) {
+					that.recordHistory();
+
+					list[index] = list.splice(index + 1, 1, list[index])[0];
+
+					that.changeModel();
+				}
+			}
+		});
+		menus.push({
+			text : "删除",
+			onClick : function() {
+				if (parentLayout == null) {
+					parentLayout = that.getLayoutRoot();
+				}
+				that.recordHistory();
+
+				let index = parentLayout.layouts.indexOf(layout);
+				parentLayout.layouts.splice(index, 1);
+
+				that.changeModel();
+
+			}
 		});
 
 
 		source.repository.contextmenu.menus = menus;
-		source.repository.contextmenu.callShow(e);
+		source.repository.contextmenu.callShow(event);
 	};
 })();
 
@@ -6577,30 +6753,64 @@ var Editor = function(options) {
 		return html;
 	};
 
-	PageEditor.prototype.buildModel = function() {};
 
-	PageEditor.prototype.showAppendModel = function($parent) {
-		this.lastAppendParent = $parent;
-		this.$modelBox.addClass('show');
-	};
-
-	PageEditor.prototype.closeAppendModel = function() {
-		this.$modelBox.removeClass('show');
-	};
-
-	PageEditor.prototype.chooseModel = function(ui, group, model, demo) {
-		let $parent = $(this.lastAppendParent);
-		if (coos.isEmpty(demo.template)) {
-			$parent.append(demo.html);
-		} else {
-			$parent.append(demo.template);
+	PageEditor.prototype.buildPageModel = function($box) {
+		if (this.buildPageModeled) {
+			return;
 		}
+		this.buildPageModeled = true;
+		let that = this;
 
-		this.changePage();
-		//		this.closeAppendModel();
+		let data = {};
+		data.show = false;
+		data.uis = this.getUIList();
+		data.ui_active_name = data.uis[0].name;
+		data.ui_group_active_name = data.ui_active_name + '-' + data.uis[0].groups[0].name;
 
+		this.page_model_data = data;
+
+		let $modelBox = $('<div class="page-editor-model-box" :class="{\'page-editor-model-box-show\':show}">' + this.getModelBoxHtml() + '</div>');
+		$box.append($modelBox);
+		new Vue({
+			el : $modelBox[0],
+			data : data,
+			methods : {
+				chooseModel (ui, group, model, demo) {
+					that.onChoosePageModel(ui, group, model, demo);
+				},
+				closeModelBox () {
+					this.show = false;
+				},
+				ui_active_change (activeName) {
+					this.$nextTick().then(res => {
+						this.uis.forEach(ui => {
+							if (activeName == (ui.name)) {
+								if (ui.lastActiveName) {
+									this.ui_group_active_name = ui.lastActiveName;
+								} else {
+									this.ui_group_active_name = ui.name + '-' + ui.groups[0].name;
+								}
+							}
+						});
+					});
+				},
+				ui_group_active_change (activeName) {}
+			}
+		});
 
 	};
+	PageEditor.prototype.choosePageModel = function(callback) {
+		this.lastChoosePageModelCallback = callback;
+		this.page_model_data.show = true;
+	};
+	PageEditor.prototype.onChoosePageModel = function(ui, group, model, demo) {
+		let layout = {};
+		layout.html = demo.html;
+		layout.template = demo.template;
+		this.lastChoosePageModelCallback && this.lastChoosePageModelCallback(layout);
+	};
+
+
 })();
 
 (function() {
